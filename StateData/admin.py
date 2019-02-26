93a01f449794ab5a3f696e56caae193dfa569194
John Hill <nouveau.pg@gmail.com>
2019-02-26 04:42:18 -0600

    basic admin for appeals data

--- StateData/admin.py
+++ StateData/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import AppealReceiptsData
 
 # Register your models here.
+
+
+class AppealReceiptsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(AppealReceiptsData, AppealReceiptsAdmin)
